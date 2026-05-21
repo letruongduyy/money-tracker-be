c012c0712fc9a7df506ed8dc24c8b7bdc7745e61
Lê Trương Duy <letruongduyy@gmail.com>
2026-05-21 13:18:08 +0700

    Added new WeeklyReportController

--- src/modules/weekly-report/weekly-report.module.ts
+++ src/modules/weekly-report/weekly-report.module.ts
@@ -4,6 +4,7 @@ import { User, UserSchema } from '../users/schemas/user.schema';
 import { TransactionsModule } from '../transactions/transactions.module';
 import { PushModule } from '../push/push.module';
 import { WeeklyReportService } from './weekly-report.service';
+import { WeeklyReportController } from './weekly-report.controller';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { WeeklyReportService } from './weekly-report.service';
     TransactionsModule,
     PushModule,
   ],
+  controllers: [WeeklyReportController],
   providers: [WeeklyReportService],
 })
 export class WeeklyReportModule {}
