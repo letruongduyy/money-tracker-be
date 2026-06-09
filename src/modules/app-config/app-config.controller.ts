f3bdefe9fe72e852e71bdb917be2f15e06989340
Lê Trương Duy <letruongduyy@gmail.com>
2026-06-09 15:10:48 +0700

    refactor: simplify app-config controller

--- src/modules/app-config/app-config.controller.ts
+++ src/modules/app-config/app-config.controller.ts
@@ -51,17 +51,11 @@ export class AppConfigController {
     );
 
     const noteBgImages = [
-      'https://i.pinimg.com/1200x/45/07/c5/4507c57013d379d050550dcb77e752c1.jpg', // None
-      'https://i.pinimg.com/webp85/736x/b8/b1/14/b8b1147c5116733231dfbb9f2bb770f8.webp',
-      'https://i.pinimg.com/736x/8c/fc/aa/8cfcaa516f47620347cd99f2ea26668b.jpg',
-      'https://i.pinimg.com/webp85/736x/95/6e/b6/956eb606ccd493d4a9efae2b8d13171e.webp',
-      'https://i.pinimg.com/webp85/1200x/d2/79/5c/d2795c371dd1e318e0dd9cda5c5b94f0.webp',
-      'https://i.pinimg.com/736x/bf/f6/5a/bff65a4bd3d92a7aef1e5db46214e7af.jpg',
-      'https://i.pinimg.com/736x/e3/57/5e/e3575e2b4b6fbfa062d586f261f06525.jpg',
-      'https://i.pinimg.com/736x/61/df/9c/61df9c2b8a063e48f55686ca300e4b67.jpg',
-      'https://i.pinimg.com/webp85/736x/61/97/6f/61976fcdb0a2ac137f4576a9f61b4ae5.webp',
-      'https://i.pinimg.com/736x/d8/f2/3c/d8f23c0520758606fce71cce2a74d87f.jpg',
-      'https://i.pinimg.com/1200x/ff/8a/56/ff8a5646cf7349564a90b36edd488c78.jpg'
+      'https://i.pinimg.com/1200x/45/07/c5/4507c57013d379d050550dcb77e752c1.jpg',
+      'https://i.pinimg.com/1200x/2c/c2/d6/2cc2d6203854347ea995c9ba3e753467.jpg',
+      'https://i.pinimg.com/1200x/ae/80/e6/ae80e67e5ca52c1d27dfde74f04f60a8.jpg',
+      'https://i.pinimg.com/1200x/a2/da/52/a2da5285ecd0650d399a11d59cd8943c.jpg'
+
     ];
 
     return {
