3fec16f43eb14f73e7d04d2740d3eb8e17863209
Lê Trương Duy <letruongduyy@gmail.com>
2026-06-09 17:12:08 +0700

    fix: add TLS options to MongoDB connection for Render SSL compatibility

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,6 +4,13 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI!)],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI!, {
+      tls: true,
+      tlsAllowInvalidCertificates: false,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+    }),
+  ],
 })
 export class DatabaseModule {}
